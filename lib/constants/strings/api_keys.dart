b38bd3cdb3590197e7032661a5b4be63abad9d63
mahmoudbakir2015 <mahmoudbakir2015@gmail.com>
2023-10-29 20:32:18 +0200

    add stripe sdk in project

---
+++ lib/constants/strings/api_keys.dart
@@ -0,0 +1,4 @@
+abstract class ApiKeys {
+  static const String stripePublishableKey =
+      'pk_test_51O6GeVIiCXsJoXqYREDwaNkgna8hJtesdFcWu8nsjkXsxCNAB3DVz2uvbLc86LFLrMEYBoxbLaC1LE4Xe7A6MTS900Q58Jjtem';
+}

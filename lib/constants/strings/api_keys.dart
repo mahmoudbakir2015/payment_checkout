42529895a1e8274fa38e036273a510907dc9c91c
mahmoudbakir2015 <76684888+mahmoudbakir2015@users.noreply.github.com>
2023-11-18 21:51:04 +0200

    Update api_keys.dart

--- lib/constants/strings/api_keys.dart
+++ lib/constants/strings/api_keys.dart
@@ -1,6 +1,6 @@
 abstract class ApiKeys {
   static const String stripePublishableKey =
-      'pk_test_51O6GeVIiCXsJoXqYREDwaNkgna8hJtesdFcWu8nsjkXsxCNAB3DVz2uvbLc86LFLrMEYBoxbLaC1LE4Xe7A6MTS900Q58Jjtem';
+      'yourPublishKey';
   static const String stripeSecretKey =
-      'sk_test_51O6GeVIiCXsJoXqYnoNVA0lMrld5m5PpvMSQV7NCSUPgSWUcz5Jra1Te61740b7m6hUFSN9Qqjokyjki2DbYnEDc00v7wAMXDq';
+      'yourSecretKey';
 }
